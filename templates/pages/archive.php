219c65cbb7af8b93bac13eaae35a90183707fdf7
paulchen <paulchen@rueckgr.at>
2018-10-04 20:02:56 +0200

    fixed wrong page title when losing focus

--- templates/pages/archive.php
+++ templates/pages/archive.php
@@ -164,7 +164,7 @@ function show_unread_message_count() {
 	if(new_messages > 0) {
 		title = '(' + new_messages + ') ';
 	}
-	title += '#zeitistkarma';
+	title += '<?php echo htmlentities($title, ENT_QUOTES, 'UTF-8') ?>';
 	$(document).prop('title', title);
 }
 
@@ -202,7 +202,9 @@ function tab_disabled() {
 		<tr><td>Page:</td><td><span style="white-space: nowrap;"><input type="text" name="page" value="<?php echo $page; ?>" /> (of <span class="page_count"><?php echo $page_count; ?></span>)</span> <span style="white-space: nowrap;"><a href="<?php echo $first_link ?>">First</a> <a href="<?php echo $previous_link ?>">Previous</a> <a href="<?php echo $next_link ?>" class="next_link">Next</a> <a href="<?php echo $last_link ?>" class="last_link">Last</a></span></td></tr>
 		<tr><td>Date:</td><td><input type="text" name="date" value="<?php if(isset($_GET['date'])) echo htmlentities($_GET['date'], ENT_QUOTES, 'UTF-8') ?>" id="date_input" /></td></tr>
 		<tr><td></td><td><input type="submit" value="Filter" /><input type="button" value="Reset" onclick="reset_form();" /></td></tr>
+<!--
 		<tr><td></td><td><input id="refresh_checkbox" type="checkbox" name="refresh" <?php if($refresh) echo 'checked="checked"'; ?> />&nbsp;<label for="refresh_checkbox">Auto-refresh every <?php echo $refresh_time ?> seconds.</label></td></tr>
+-->
 		</table>
 		<input type="hidden" name="jodel" value="<?php echo $jodel ?>" />
 		</form>
